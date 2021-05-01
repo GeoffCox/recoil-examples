cec2d95223f6158618238dfc0754c88eced56afb
GeoffCox <geoff.cox@live.com>
2021-05-01 12:18:52 -0700

    Update TodoList2.tsx

--- atoms-tutorial/src/components/TodoList2.tsx
+++ atoms-tutorial/src/components/TodoList2.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { useRecoilValue } from "recoil";
 import { todoListState } from "../atoms";
-import { TodoItemCreator } from "./TodoItemCreator";
-import { TodoItem } from "./TodoItem";
+import { TodoItemCreator } from "./TodoItemCreator2";
+import { TodoItem } from "./TodoItem2";
 
 export const TodoList = () => {
   const todoList = useRecoilValue(todoListState);
