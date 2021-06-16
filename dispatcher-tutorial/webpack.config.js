a7150dbaec17b491b27396d28e7eddc1abd488c2
GeoffCox <geoff.cox@live.com>
2021-06-16 10:48:13 -0700

    Updated to latest webpack

--- dispatcher-tutorial/webpack.config.js
+++ dispatcher-tutorial/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loaders: ["awesome-typescript-loader"] },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js?$/, loader: "source-map-loader" },
